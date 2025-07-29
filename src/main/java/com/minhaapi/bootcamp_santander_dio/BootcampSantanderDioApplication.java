d9c3ca428ab662be7abcfcfcb3ce47c8767a6113
AndersonnTech <anderson2csilva98@gmail.com>
2025-07-29 10:56:34 -0300

    Initial commit

---
+++ src/main/java/com/minhaapi/bootcamp_santander_dio/BootcampSantanderDioApplication.java
@@ -0,0 +1,13 @@
+package com.minhaapi.bootcamp_santander_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantanderDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantanderDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhaapi/bootcamp_santander_dio/BootcampSantanderDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaapi.bootcamp_santander_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantanderDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
